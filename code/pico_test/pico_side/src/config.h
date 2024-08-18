1c5468bc92f02eca2a7af919f0c440a9b35b6ed0
Leander <leander.funken@live.de>
2024-08-18 10:44:58 +0200

    Refactor

--- code/pico_test/pico_side/src/config.h
+++
@@ -1,8 +0,0 @@
-#include <hardware/gpio.h>
-#include <hardware/spi.h>
-#include <pico/binary_info/code.h>
-#include <pico/stdio.h>
-#include <pico/stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <tusb.h>
--- code/pico_test/pico_side/src/util.h
+++
@@ -1,5 +0,0 @@
-#include "config.h"
-
-int pico_read(char* readBuffer, int size);
-
-void pico_print(char* message);
