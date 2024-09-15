d2eaf32e13c7b9b64b9edd33c0a0a5cbeb784e2d
Leander <leander.funken@live.de>
2024-09-15 11:59:25 +0200

    redid diagnostics

--- code/general/diagnostics/include/diagnostics.h
+++ code/general/diagnostics/include/diagnostics.h
@@ -1,1 +1,34 @@
+#pragma once
 #include <stdint.h>
+
+typedef union {
+    float f;
+    uint8_t bytes[4];
+}FloatBytes;
+
+typedef union {
+    int i;
+    uint8_t bytes[4];
+}IntBytes;
+
+typedef struct {
+    float transmissionsPerSecond;
+    int picoReadTimeouts;
+    int usbDisconnects;
+    int usbReadErrors;
+    int usbWriteErrors;
+}PCSystemLog;
+
+typedef struct {
+    FloatBytes transmissionsPerSecond;
+    IntBytes usbDisconnects;
+    IntBytes readTimeouts;
+}PicoSystemLog;
+
+void read_float_bytes_f(FloatBytes* fb, float f);
+
+void read_float_bytes_b(FloatBytes* fb, uint8_t bytes[4]);
+
+void read_int_bytes_i(IntBytes* ib, int i);
+
+void read_int_bytes_b(IntBytes* ib, uint8_t bytes[4]);
