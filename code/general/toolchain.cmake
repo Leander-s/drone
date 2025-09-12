7004e9a1a57a50aecb6cd073433e9efbdaaac07d
Leander Funken <leander.funken@gmail.com>
2025-09-12 06:59:27 +0200

    compiler integrated in project

---
+++ code/general/toolchain.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(TOOLCHAIN_DIR "${CMAKE_SOURCE_DIR}/tools/toolchain/bin")
+set(CMAKE_C_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-gcc")
+
+set(CMAKE_OBJCOPY "${TOOLCHAIN_DIR}/arm-none-eabi-objcopy")
+set(CMAKE_SIZE "${TOOLCHAIN_DIR}/arm-none-eabi-size")
