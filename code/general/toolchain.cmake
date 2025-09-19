682c6ec579baf3ceba0b6ecca2b88998811625e5
Leander Funken <leander.funken@gmail.com>
2025-09-19 06:49:25 +0200

    build update, still issues with cmake

--- code/general/toolchain.cmake
+++ code/general/toolchain.cmake
@@ -1,10 +1,12 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(TOOLCHAIN_DIR "${CMAKE_SOURCE_DIR}/tools/toolchain/bin")
+set(TOOLCHAIN_DIR "${CMAKE_SOURCE_DIR}/../tools/toolchain/bin")
 set(CMAKE_C_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-g++")
 set(CMAKE_ASM_COMPILER "${TOOLCHAIN_DIR}/arm-none-eabi-gcc")
 
 set(CMAKE_OBJCOPY "${TOOLCHAIN_DIR}/arm-none-eabi-objcopy")
 set(CMAKE_SIZE "${TOOLCHAIN_DIR}/arm-none-eabi-size")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
