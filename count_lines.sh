40ee280c559fe99a1498fe392659ee519ae6eace
Leander Funken <leander.funken@gmail.com>
2025-12-23 16:19:57 +0100

    feat: init and build script
    using the code/init script you can now initialize the
     repository to be able to work in it and build it properly. Using the build script you can now build all three projects in the
    repository

--- count_lines.sh
+++
@@ -1,1 +0,0 @@
-wc -l $(find -type f ! -path "*include*" ! -path "*build/*" ! -path "*debug/*" ! -path "*glad*" ! -path "*cglm*" ! -path "*pico-sdk*" ! -path "*SDL*" \( -name "*.h" -o -name "*.c" \))
