8b2ff6d748b8dafe834f5e7433800d5af984c876
Leander Funken <leander.funken@gmail.com>
2025-12-27 16:19:26 +0100

    feat: build script now has help. You can specify a project to build

--- code/drone_deploy.sh
+++ code/drone_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-(cd ./drone/build/ && make)
+./build drone
 
 if [ -d "/media/leander/RPI-RP2" ]
 then
-    sudo cp ./drone/build/drone.uf2 /media/leander/RPI-RP2/
+    sudo cp ./bin/drone.uf2 /media/leander/RPI-RP2/
 else
     sudo mkdir /media/leander/RPI-RP2
     sudo mount /dev/sdd1 /media/leander/RPI-RP2
-    sudo cp ./drone/build/drone.uf2 /media/leander/RPI-RP2/
+    sudo cp ./bin/drone.uf2 /media/leander/RPI-RP2/
     sudo umount /media/leander/RPI-RP2
     sudo rm -rf /media/leander/RPI-RP2
 fi
